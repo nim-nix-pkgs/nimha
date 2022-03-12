0cb88a13e9c1aabe784f041219cf2dd1058eb6e7
hugosenari <hugosenari@gmail.com>
2022-03-12 04:28:05 -0300

    chore: re index nimha

---
+++ flake.nix
@@ -0,0 +1,67 @@
+{
+  description = ''Nim Home Assistant (NimHA) is a hub for combining multiple home automation devices and automating jobs'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."nimha-master".url = "path:./master";
+  inputs."nimha-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nimha-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."nimha-v0_3_0".url = "path:./v0_3_0";
+  inputs."nimha-v0_3_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nimha-v0_3_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."nimha-v0_3_1".url = "path:./v0_3_1";
+  inputs."nimha-v0_3_1".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nimha-v0_3_1".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."nimha-v0_3_2".url = "path:./v0_3_2";
+  inputs."nimha-v0_3_2".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nimha-v0_3_2".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."nimha-v0_3_3".url = "path:./v0_3_3";
+  inputs."nimha-v0_3_3".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nimha-v0_3_3".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."nimha-v0_3_5".url = "path:./v0_3_5";
+  inputs."nimha-v0_3_5".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nimha-v0_3_5".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."nimha-v0_3_6".url = "path:./v0_3_6";
+  inputs."nimha-v0_3_6".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nimha-v0_3_6".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."nimha-v0_4_0".url = "path:./v0_4_0";
+  inputs."nimha-v0_4_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nimha-v0_4_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."nimha-v0_4_1".url = "path:./v0_4_1";
+  inputs."nimha-v0_4_1".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nimha-v0_4_1".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."nimha-v0_4_3".url = "path:./v0_4_3";
+  inputs."nimha-v0_4_3".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nimha-v0_4_3".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."nimha-v0_4_4".url = "path:./v0_4_4";
+  inputs."nimha-v0_4_4".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nimha-v0_4_4".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."nimha-v0_4_5".url = "path:./v0_4_5";
+  inputs."nimha-v0_4_5".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nimha-v0_4_5".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ v0_4_3/flake.nix
@@ -0,0 +1,90 @@
+{
+  description = ''Nim Home Assistant (NimHA) is a hub for combining multiple home automation devices and automating jobs'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-nimha-v0_4_3.flake = false;
+  inputs.src-nimha-v0_4_3.owner = "ThomasTJdev";
+  inputs.src-nimha-v0_4_3.ref   = "refs/tags/v0.4.3";
+  inputs.src-nimha-v0_4_3.repo  = "nim_homeassistant";
+  inputs.src-nimha-v0_4_3.type  = "github";
+  
+  inputs."jester".dir   = "nimpkgs/j/jester";
+  inputs."jester".owner = "riinr";
+  inputs."jester".ref   = "flake-pinning";
+  inputs."jester".repo  = "flake-nimble";
+  inputs."jester".type  = "github";
+  inputs."jester".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."jester".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."httpbeast".dir   = "nimpkgs/h/httpbeast";
+  inputs."httpbeast".owner = "riinr";
+  inputs."httpbeast".ref   = "flake-pinning";
+  inputs."httpbeast".repo  = "flake-nimble";
+  inputs."httpbeast".type  = "github";
+  inputs."httpbeast".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."httpbeast".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."recaptcha".dir   = "nimpkgs/r/recaptcha";
+  inputs."recaptcha".owner = "riinr";
+  inputs."recaptcha".ref   = "flake-pinning";
+  inputs."recaptcha".repo  = "flake-nimble";
+  inputs."recaptcha".type  = "github";
+  inputs."recaptcha".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."recaptcha".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."bcrypt".dir   = "nimpkgs/b/bcrypt";
+  inputs."bcrypt".owner = "riinr";
+  inputs."bcrypt".ref   = "flake-pinning";
+  inputs."bcrypt".repo  = "flake-nimble";
+  inputs."bcrypt".type  = "github";
+  inputs."bcrypt".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."bcrypt".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."multicast".dir   = "nimpkgs/m/multicast";
+  inputs."multicast".owner = "riinr";
+  inputs."multicast".ref   = "flake-pinning";
+  inputs."multicast".repo  = "flake-nimble";
+  inputs."multicast".type  = "github";
+  inputs."multicast".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."multicast".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."websocket".dir   = "nimpkgs/w/websocket";
+  inputs."websocket".owner = "riinr";
+  inputs."websocket".ref   = "flake-pinning";
+  inputs."websocket".repo  = "flake-nimble";
+  inputs."websocket".type  = "github";
+  inputs."websocket".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."websocket".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."wiringpinim".dir   = "nimpkgs/w/wiringpinim";
+  inputs."wiringpinim".owner = "riinr";
+  inputs."wiringpinim".ref   = "flake-pinning";
+  inputs."wiringpinim".repo  = "flake-nimble";
+  inputs."wiringpinim".type  = "github";
+  inputs."wiringpinim".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."wiringpinim".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."xiaomi".dir   = "nimpkgs/x/xiaomi";
+  inputs."xiaomi".owner = "riinr";
+  inputs."xiaomi".ref   = "flake-pinning";
+  inputs."xiaomi".repo  = "flake-nimble";
+  inputs."xiaomi".type  = "github";
+  inputs."xiaomi".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."xiaomi".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-nimha-v0_4_3"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-nimha-v0_4_3";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
